66df571eed333f87afa6127e1108e644d7f20b8b
Shirel-cohen <shireli1234c@gmail.com>
2023-11-05 16:57:51 +0200

    Update

--- app.py
+++ app.py
@@ -3,31 +3,43 @@ from deepface import DeepFace
 
 app = Flask(__name__)
 
-# @app.route('/', methods=['GET'])
-# def analyze_mood():
-#     face_analysis = DeepFace.analyze(img_path="sad.jpg")
-#     return face_analysis
-#
-#
-# app.run(host = '0.0.0.0')
 
-@app.route('/', methods=['POST'])
+def filter_unnecessary_emotion_and_take_close_one(emotion_dictionary):
+    del emotion_dictionary["fear"]
+    del emotion_dictionary["disgust"]
+    second_close_emotion = max(emotion_dictionary, key=emotion_dictionary.get)
+    return second_close_emotion
+
+    # print(second_dominant_emotion)
+    # return jsonify(mod=emotion_dictionary[max_key])
+    # max_value = emotion_dictionary[max_key]
+    # for key, value in emotion_dictionary.items():
+    #     print(f"key: {key} , value: {value}")
+    # print("Key with highest value:", max_key)
+    # print("Highest value:", max_value)
+
+
+@app.route('/', methods=['POST', 'GET'])
 def analyze_mood():
     if 'image' not in request.files:
-        return "No image provided", 400
+        return jsonify({'error': 'No image part'})
 
     image_file = request.files['image']
 
     if image_file.filename == '':
-        return "No selected image", 400
+        return jsonify({'error': 'No selected image'})
 
-    image_path = "temp_image.jpg"  # Temporary image path
+    # image_path : to save/image.jpg
+    image_path = "temp_image.jpg"
     image_file.save(image_path)
+    face_analysis = DeepFace.analyze(img_path=image_path, actions=['emotion'])
+    face_emotion = face_analysis[0]["dominant_emotion"]
+    if face_emotion == 'fear' or face_emotion == 'disgust':
+        emotion_dictionary = face_emotion[0]['emotion']
+        face_emotion = filter_unnecessary_emotion_and_take_close_one(emotion_dictionary)
 
-    face_analysis = DeepFace.analyze(img_path=image_path)
+    return jsonify(mod=face_emotion)
 
-    # You might want to format the response as needed
-    # For example: return jsonify({"mood": face_analysis["dominant_emotion"]})
-    return jsonify(face_analysis)
 
-app.run(host='0.0.0.0')
+if __name__ == '__main__':
+    app.run()
